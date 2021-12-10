9976fff2a4a4bce19f8596d5d4422ff079787bc1
Isabella Mighell <ikm4dm@virginia.edu>
2021-12-10 16:10:02 -0500

    had different solution and two loops but did it by individual character

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,20 @@
 function romanNumeral(string) {
   // type your code here
+  const romanNumerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000 }
+  let numArr = []
+  for(let i=0; i<string.length; i++){
+    const num = romanNumerals[string[i]]
+    numArr.push(num)
+  } //convert letters to numbers
+
+  for(let i=0; i <numArr.length-1; i++){
+    if(numArr[i]<numArr[i+1]){
+      numArr[i] = numArr[i]*-1
+    }
+  }
+
+  const total = numArr.reduce((total, num)=> total+num)
+  return total
 }
 
 if (require.main === module) {
